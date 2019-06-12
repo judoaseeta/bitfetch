f4c9220c37e12e20ed8101a2d87417ff0b969faf
byeongjunchoi <judoaseeta@gmail.com>
2019-06-13 01:14:03 +0900

    remove all redux related code since it doesn't need to share its state globally.

--- src/containers/AreaChart.tsx
+++ src/containers/AreaChart.tsx
@@ -1,59 +1,13 @@
 import * as React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch, Reducer} from 'redux';
-import { Epic,  ofType } from 'redux-observable';
-import { mergeMap, tap , map } from 'rxjs/operators';
+
 import {scaleLinear, ScaleLinear, scaleTime, ScaleTime} from 'd3-scale';
 
-import { requestTopCoinData } from '../utils/api';
+import {createCurrentSubcription, createSocketCoin, requestTopCoinData} from '../utils/api';
 import {createAction, ActionsUnion, ActionWithPayload} from '../utils/createAction';
 import { RootState } from './';
-import {from} from "rxjs";
-enum actionTypes {
-    REQUEST_TOP_LIST = 'REQUEST_TOP_LIST',
-    LISTEN_TOP_LIST_COINS = 'LISTEN_TOP_LIST_COINS '
-}
-interface TopListCoinReq {
-    currentCoinSymbol: string;
-    histo: string;
-    tsym?: string;
-}
-const dispatchers = {
-    REQUEST_TOP_LIST: (payload: TopListCoinReq) => createAction(actionTypes.REQUEST_TOP_LIST, payload),
-    LISTEN_TOP_LIST_COINS: (payload: MultiCoinResp[]) => createAction(actionTypes.LISTEN_TOP_LIST_COINS, payload),
-};
-type Actions = ActionsUnion<typeof dispatchers>;
-const RequestTopListEpic: Epic<Actions> = (action$) => action$.pipe(
-    ofType<Actions, ActionWithPayload<actionTypes.REQUEST_TOP_LIST, TopListCoinReq>>(actionTypes.REQUEST_TOP_LIST),
-    mergeMap(d =>
-        from(requestTopCoinData(d.payload.currentCoinSymbol, d.payload.histo, d.payload.tsym)).pipe(
-            tap(d => console.log(d)),
-            map(d => dispatchers.LISTEN_TOP_LIST_COINS(d))
-        )
-    )
-);
-export const epics = [ RequestTopListEpic ];
-type RState = {
-    coinDatas: {
-        name: string;
-        data: CoinHistoData[]
-    }[] | null;
-}
-const initialState = {
-    coinDatas : null
-};
-export const reducer: Reducer<RState, Actions> = (state = initialState, action) => {
-    return state;
-};
-const mapStateToProps = (state: RootState) => {
-    return {
-        data: state.area.coinDatas
-    }
-};
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    ...bindActionCreators({...dispatchers}, dispatch)
-});
+
 
 export type RenderProp = State;
 export type Props = {
@@ -61,9 +15,7 @@ export type Props = {
     currentCoinSymbol: string;
     histo: string;
     tsym?: string;
-} & ReturnType<typeof mapDispatchToProps>
-& ReturnType<typeof mapStateToProps>
-& RouteComponentProps<{}>;
+} & RouteComponentProps<{}>;
 export type State = {
     xScale: ScaleTime<Date, number>;
     yScale: ScaleLinear<number, number>;
@@ -75,10 +27,7 @@ class AreaChart extends React.Component<Props, State> {
         yScale: scaleLinear()
     };
     componentDidMount() {
-        const { REQUEST_TOP_LIST,  histo, currentCoinSymbol , tsym} =  this.props;
-        if(!this.props.data) {
-            REQUEST_TOP_LIST({ currentCoinSymbol, histo, tsym});
-        }
+
     }
     render() {
         return this.props.children({
@@ -87,4 +36,4 @@ class AreaChart extends React.Component<Props, State> {
     }
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AreaChart));
+export default withRouter(AreaChart);
